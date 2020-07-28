5cc3e065a9482f6592381ce8f2bc672896f3d190
Tyler Rutt <Tylerrutt17@gmail.com>
2020-07-28 14:03:06 -0400

    db functions

--- dbFunctions/dbfunctions.js
+++ dbFunctions/dbfunctions.js
@@ -1,11 +1,15 @@
 const bcrypt = require('bcrypt')
 
-const uploadNewUser = (db, name, username, favorite_foods, zipcode, rawpassword, callback)=>{
+const pgp = require('pg-promise')()
+
+// Link to the database
+const db = pgp('postgres://dghqeslf:mNRbeXOviur1ep7cTdIZ2Gt0lzDs2UNi@ruby.db.elephantsql.com:5432/dghqeslf')
+
+const uploadNewUser = async (db, name, username, favorite_foods, zipcode, rawpassword, callback)=>{
     // Hashes the raw password using bcrypt.
     const hashedPassword = await bcrypt.hash(rawpassword, 10)
 
-    // Username is unique
-
+    // Username is unique, will error out if it conflicts with a previous username
     db.none(`INSERT INTO users (name, username, favorite_foods, zipcode, password) VALUES ('${name}', '${username}', '${favorite_foods}', '${zipcode}', '${hashedPassword}')`)
     .then(()=>{
         console.log("Successfully created and instantiated user into the DB!")
@@ -14,16 +18,15 @@ const uploadNewUser = (db, name, username, favorite_foods, zipcode, rawpassword,
     .catch(() => console.log("Error Uploading User To DB. :("))
 }
 
-const loadUser = (db, username) => {
+// On login, load users information
+const loadUser = async (db, username) => {
     db.one(`SELECT * FROM users WHERE username = '${username}'`)
     .then(user =>{
-        console.log('loaded')
+        console.log(`loaded user ${user.favorite_foods}`)
         return user
     })
     .catch(err=>console.log("Error finding that user...", err))
 }
-
-
 
 module.exports = {
     uploadNewUser, loadUser
