54133981ef8e0ac59d222fcb77f9244518a7d764
Rashad Collins <rashad.collins22@gmail.com>
2020-07-28 13:30:15 -0400

    first

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 require("./api-routes")(app);//sets the api
 
+const apiKey = "e1igrlOBCl2OLF2gvBQxvruvm8U0p1r_fyb-J6ELqw9zldWhE8YpTAHlXyXezJWxI6L-vYl5OB1mC9GvcFS_NGXlDm9RBG8hlv_7HN2wnnL5nboFkfC4-i7sOisgX3Yx"
+
+
+
 app.use(express.static("public"));
 
 
@@ -9,3 +13,13 @@ const port = 5434;
 app.listen(port, ()=>{
     console.log(`listening on http://localhost:${port}`)
 })
+
+const firstSearch = () => {
+	fetch("https://api.yelp.com/v3/businesses/search?location=30326&price=1", {
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+    })
+}
+
+firstSearch()
