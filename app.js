9f3df0d7b890fa9268b172068f581e1add6c2072
Tyler Rutt <Tylerrutt17@gmail.com>
2020-07-29 09:33:17 -0400

    new commit

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const app = express();
 require("./api-routes")(app);//sets the api
 
 app.use(express.static("public"));
+app.use(express.static("dbFunctions"));
 
 
-const port = 5434;
+const port = 5000;
 app.listen(port, ()=>{
     console.log(`listening on http://localhost:${port}`)
 })
