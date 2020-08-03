64d3220112eceb853130278d124e0fb6b07505f6
Tyler Rutt <Tylerrutt17@gmail.com>
2020-08-03 18:23:35 -0400

    new login

--- js/profile.js
+++ js/profile.js
@@ -1,33 +1,44 @@
-const writeReview = document.querySelector(".submitbutton");
-const uploadPicture = document.querySelector(".submitpicture");
+const start = (user) => {
 
+  console.log(user)
 
-import {currentUser} from './app.js'
+  const writeReview = document.querySelector(".submitbutton");
+  const uploadPicture = document.querySelector(".submitpicture");
 
-
-function visi() {
+  function visi() {
   var reviewPageOpen = document.getElementById("review");
   if (reviewPageOpen.style.display === "none") {
     reviewPageOpen.style.display = "block";
   } else {
     reviewPageOpen.style.display = "none";
   }
-}
-writeReview.addEventListener("click", visi);
-
-function visi2() {
-  var pic = document.getElementById("picture");
-  if (pic.style.display === "none") {
-    pic.style.display = "block";
-  } else {
-    pic.style.display = "none";
   }
+    writeReview.addEventListener("click", visi);
+
+  function visi2() {
+    var pic = document.getElementById("picture");
+      if (pic.style.display === "none") {
+        pic.style.display = "block";
+      } else {
+        pic.style.display = "none";
+      }
+  }
+
+  uploadPicture.addEventListener("click", visi2);
+
+  let usernameLbl = document.querySelector('.username')
+  usernameLbl.innerText = user.name
+  let locationLbl = document.querySelector('.location')
+  locationLbl.innerText = `Zipcode: ${user.zipcode}`
+
+  let userwelcomeLbl = document.querySelector('.userwelcome')
+  userwelcomeLbl.innerText = `Let's Eat, ${user.name}`
 }
 
-uploadPicture.addEventListener("click", visi2);
+fetch(`/me/${localStorage.getItem('username')}`)
+.then(data=>data.json())
+.then(user=>start(user))
 
-let usernameLbl = document.getElementsByClassName('username')
-usernameLbl.innerText = currentUser.name
+//start()
 
-console.log("The Name is", user.name)
 
